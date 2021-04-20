e436359ed79f9a34d7b8707de54a267c49261577
Ben Usher <ben.usher@triptease.com>
2021-04-20 14:26:35 +0100

    Create multiple messages and test all are returned.
    Co-authored-by: Hannes Crombez <hcr@users.noreply.github.com>

--- server/test/Message.integration.test.ts
+++ server/test/Message.integration.test.ts
@@ -2,28 +2,44 @@ import { App } from '../src/App';
 import fetch from 'node-fetch';
 
 describe('Message', () => {
-  it('Creates and retrieves all message', async () => {
+  it('Creates and retrieves all messages', async () => {
     const port = 6000;
     const app = new App(port);
     await app.start();
 
-    const message = 'message-' + Math.round(Math.random() * 1_000_000_000);
+    const message1 = 'message-' + Math.round(Math.random() * 1_000_000_000);
+    const message2 = 'message-' + Math.round(Math.random() * 1_000_000_000);
 
     try {
-      const postResult = await fetch(`http://localhost:${port}/messages`, {
+      const postResult1 = await fetch(`http://localhost:${port}/messages`, {
         method: 'POST',
-        body: message,
+        body: message1,
       });
-      expect(postResult.status).toBe(201);
+      expect(postResult1.status).toBe(201);
+
+      const postResult2 = await fetch(`http://localhost:${port}/messages`, {
+        method: 'POST',
+        body: message2,
+      });
+      expect(postResult2.status).toBe(201);
 
       const getResult = await fetch(`http://localhost:${port}/messages`, { method: 'GET' });
 
       expect(getResult.status).toBe(200);
       const messages = await getResult.json();
-      expect(messages).toContainEqual({
-        id: expect.stringMatching(/^.+$/),
-        message,
-      });
+
+      expect(messages).toEqual(
+        expect.arrayContaining([
+          {
+            id: expect.stringMatching(/^.+$/),
+            message: message1,
+          },
+          {
+            id: expect.stringMatching(/^.+$/),
+            message: message2,
+          },
+        ]),
+      );
     } finally {
       await app.stop();
     }
